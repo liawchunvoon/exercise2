fb194ee9faf1d7c56bb9b59b70a3c8b2ca373c99
Liaw Chun Voon <39509476+liawchunvoon@users.noreply.github.com>
2025-02-05 16:04:46 +0800

    updated

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+import pandas as pd
+import openai
+from openai.embeddings_utils import cosine_similarity
+
+openai.api_key = st.secrets["mykey"]
+
+# Function to get embedding
+def get_embedding(text, model="text-embedding-ada-002"):
+   return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
+
+
 import ast
 df = pd.read_csv("qa_dataset_with_embeddings.csv")
 
@@ -23,3 +34,12 @@ def find_best_answer(user_question):
       return best_answer
    else:
       return "I apologize, but I don't have information on that topic yet. Could you please ask other questions?"
+
+
+import streamlit as st
+
+st.title("Exercise 2")
+question = st.text_input("Question")
+if st.button("Answer"):
+   answer = find_best_answer(question)
+   st.write(answer)
